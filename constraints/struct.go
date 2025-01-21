fa64db2ce0546c07783618ea8cfb6478b07c976b
Jan Galek <admin@gcore.cz>
2025-01-21 03:11:18 +0100

    [Add] struct constraint

--- constraints/struct.go
+++ constraints/struct.go
@@ -1,1 +1,43 @@
 package constraints
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type IsStruct struct{}
+
+type Struct struct {
+	Struct interface{}
+}
+
+func (c IsStruct) Validate(value any) error {
+	v := reflect.ValueOf(value)
+	if v.Kind() != reflect.Struct {
+		return errors.New("this value must be a struct")
+	}
+
+	return nil
+}
+
+func (c Struct) Validate(value any) error {
+	v := reflect.ValueOf(value)
+	if v.Kind() != reflect.Struct {
+		return errors.New("this value must be a struct")
+	}
+
+	expectedType := reflect.TypeOf(c.Struct)
+	if expectedType.Kind() == reflect.Struct {
+		for i := 0; i < expectedType.NumField(); i++ {
+			fieldName := expectedType.Field(i).Name
+			fieldValue := v.FieldByName(fieldName)
+
+			if !fieldValue.IsValid() {
+				return fmt.Errorf("struct does not contain field '%s'", fieldName)
+			}
+		}
+	}
+
+	return nil
+}
--- tests/constraint_struct_test.go
+++ tests/constraint_struct_test.go
@@ -1,1 +1,93 @@
 package tests
+
+import (
+	"github.com/gouef/validator"
+	"github.com/gouef/validator/constraints"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type MyStruct struct {
+	Field1 string
+	Field2 int
+}
+
+type InvalidStruct struct {
+	Field1 string
+}
+
+func TestStructValidator(t *testing.T) {
+	validStruct := MyStruct{
+		Field1: "Valid",
+		Field2: 123,
+	}
+
+	invalidStruct := InvalidStruct{
+		Field1: "Invalid",
+	}
+
+	invalidStruct2 := struct {
+		Field2 int
+	}{
+		Field2: 456,
+	}
+
+	con := constraints.Struct{
+		Struct: MyStruct{},
+	}
+
+	tests := []struct {
+		name        string
+		value       any
+		constraints []validator.Constraint
+		expectErr   bool
+	}{
+		{
+			name:        "Valid Struct constraint",
+			value:       validStruct,
+			constraints: []validator.Constraint{con},
+			expectErr:   false,
+		},
+		{
+			name:        "Invalid Struct constraint",
+			value:       invalidStruct,
+			constraints: []validator.Constraint{con},
+			expectErr:   true,
+		},
+		{
+			name:        "Invalid Struct 2 constraint",
+			value:       invalidStruct2,
+			constraints: []validator.Constraint{con},
+			expectErr:   true,
+		},
+		{
+			name:        "Invalid Struct constraint (not struct)",
+			value:       "string",
+			constraints: []validator.Constraint{con},
+			expectErr:   true,
+		},
+		{
+			name:        "Valid IsStruct constraint",
+			value:       validStruct,
+			constraints: []validator.Constraint{constraints.IsStruct{}},
+			expectErr:   false,
+		},
+		{
+			name:        "Invalid IsStruct constraint",
+			value:       65,
+			constraints: []validator.Constraint{constraints.IsStruct{}},
+			expectErr:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			errs := validator.Validate(tt.value, tt.constraints...)
+			if tt.expectErr {
+				assert.NotEmpty(t, errs, "Expected error but got none")
+			} else {
+				assert.Empty(t, errs, "Expected no error but got some")
+			}
+		})
+	}
+}
